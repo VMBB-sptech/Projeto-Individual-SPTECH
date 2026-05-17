f76fa6e34d3e01a32712ecda705dba1a8a31d6e9
VMBB-sptech <victor.bertolino@sptech.school>
2026-05-17 12:31:09 -0300

    terminando de renderizar as KPI's

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var tentativasRouter = require("./src/routes/tentativas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +25,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/tentativas", tentativasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
